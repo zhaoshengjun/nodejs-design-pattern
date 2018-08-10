781331e4751a87d59163e88c199e7c55bc2341b5
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-10 23:21:11 +1000

    singleton setup

---
+++ singleton/index.js
@@ -0,0 +1,17 @@
+const Logger = require("./logger");
+const Shopper = require("./shopper");
+const Store = require("./store");
+
+let logger = new Logger();
+logger.log("Starting app...");
+
+let alex = new Shopper("alex", 500);
+let ski_shop = new Store("Steeep and Deep Supplies", [
+  { item: "Downhill Skies", qty: 5, price: 750 },
+  { item: "Knit Hat", qty: 20, price: 5 }
+]);
+
+logger.log(`Finishing config...`);
+
+console.log(`${logger.count} logs total`);
+logger.logs.map(log => console.log(`${log.message}`));
---
+++ singleton/shopper.js
@@ -0,0 +1,12 @@
+const Logger = require("./logger");
+let logger = new Logger();
+
+class Shopper {
+  constructor(name, money = 0) {
+    this.name = name;
+    this.money = money;
+    logger.log(`New Shopper: ${name} has ${money} in hand.`);
+  }
+}
+
+module.exports = Shopper;
