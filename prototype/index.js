03ae63af85620ce6353e3a536adeb1668f9a0daa
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-11 21:46:19 +1000

    prototype setup

---
+++ prototype/index.js
@@ -0,0 +1,18 @@
+const Shopper = require("./shopper");
+
+let alex = new Shopper("Alex Banks");
+alex.addItemToList("camping knife");
+alex.addItemToList("tent");
+alex.addItemToList("backpack");
+alex.addItemToList("map");
+alex.addItemToList("singlshot");
+
+let eve = new Shopper("Eve Porcello");
+eve.addItemToList("camping knife");
+eve.addItemToList("tent");
+eve.addItemToList("backpack");
+eve.addItemToList("map");
+eve.addItemToList("reading light");
+
+console.log(`${alex.name}: ${alex.shoppingList}`);
+console.log(`${eve.name}: ${eve.shoppingList}`);
