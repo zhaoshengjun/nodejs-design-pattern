7200c86c968bb0cb3c9dc22bf655e05f0839bc96
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-12 23:29:15 +1000

    factory

--- factory/index.js
+++ factory/index.js
@@ -1,8 +1,7 @@
-const Shopper = require("./shopper");
-const Employee = require("./employee");
+const userFactory = require("./userFactory");
 
-let alex = new Shopper("Alex Banks", 100);
-let eve = new Employee("Eve Porecllo", 100, "this and that");
+let alex = userFactory("Alex Banks", 100);
+let eve = userFactory("Eve Porecllo", 100, "employee", "this and that");
 
 console.log(alex.toString());
 console.log(eve.toString());
---
+++ factory/userFactory.js
@@ -0,0 +1,12 @@
+const Shopper = require("./shopper");
+const Employee = require("./employee");
+
+const userFactory = (name, money = 0, type, employer) => {
+  if (type === "employee") {
+    return new Employee(name, money, employer);
+  } else {
+    return new Shopper(name, money);
+  }
+};
+
+module.exports = userFactory;
