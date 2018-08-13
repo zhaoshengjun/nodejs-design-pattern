349b149f242d45ad00b8c7d6a70ded89ff38b865
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-13 23:19:24 +1000

    builder setup

---
+++ builder/index.js
@@ -0,0 +1,16 @@
+const Person = require("./person");
+
+// Employees
+let sue = new Person("Sue", true, true, 60);
+let bill = new Person("Bill", true, false, 20);
+let phil = new Person("Phil", true, false);
+
+// Shoppers
+let charles = new Person(
+  "Charles",
+  false,
+  false,
+  0,
+  (500)[("Jean", "Sunglass")]
+);
+let tabbitha = new Person("Tabbitha", false, false, 0, 1000);
