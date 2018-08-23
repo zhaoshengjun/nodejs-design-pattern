bba8af3f6dac0a9db501d928a14d9f2e5051e363
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-23 22:19:32 +1000

    add command history

--- command/conductor.js
+++ command/conductor.js
@@ -1,7 +1,16 @@
 class Conductor {
+  constructor() {
+    this.history = [];
+  }
   run(command) {
     console.log(`Executing command: ${command.name}`);
+    this.history.push(command);
+    this.printHistory();
     command.execute();
+  }
+
+  printHistory() {
+    this.history.forEach(cmd => console.log(cmd.name));
   }
 }
 
