a133995cff3b7868a324f371aedef5f8abca09fd
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-24 22:18:35 +1000

    implement iterator

---
+++ iterator/iterator.js
@@ -0,0 +1,35 @@
+class Iterator {
+  constructor(items = []) {
+    this.index = 0;
+    this.items = items;
+  }
+
+  first() {
+    let [first] = this.items;
+    return first;
+  }
+  last() {
+    let [last] = [...this.items].reverse();
+    return last;
+  }
+  next() {
+    if (this.hasNext()) {
+      this.index += 1;
+    }
+    return this.current();
+  }
+  prev() {
+    if (this.index > 0) {
+      this.index -= 1;
+    }
+    return this.current();
+  }
+  hasNext() {
+    return this.index < this.items.length - 1;
+  }
+  current() {
+    return this.items[this.index];
+  }
+}
+
+module.exports = Iterator;
