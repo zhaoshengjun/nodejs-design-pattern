1fc9c1d0678b8d86a33540ede8aa28881fc19ade
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-24 22:07:26 +1000

    iterator setup

---
+++ iterator/inventoryItem.js
@@ -0,0 +1,11 @@
+class InventoryItem {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  writeLn() {
+    process.stdout.write(`${this.name}:$${this.price}`);
+  }
+}
+
+module.exports = InventoryItem;
