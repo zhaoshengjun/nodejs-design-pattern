7a42af530028429fd6df73d6abff8c21f774efd7
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-25 22:49:57 +1000

    observer setup

---
+++ observer/index.js
@@ -0,0 +1,24 @@
+const Store = require("./store");
+const Shopper = require("./shopper");
+const Mall = require("./mall");
+
+let catsAndThings = new Store("Cats & Things");
+let insAndOuts = new Store("Ins and Outs");
+let alex = new Shopper("Alex");
+let eve = new Shopper("Eve");
+let sharon = new Shopper("Sharon");
+let mike = new Shopper("Mike");
+let valleyMall = new Mall();
+
+catsAndThings.subscribe(alex);
+catsAndThings.subscribe(eve);
+catsAndThings.subscribe(mike);
+catsAndThings.subscribe(valleyMall);
+
+insAndOuts.subscribe(sharon);
+insAndOuts.subscribe(valleyMall);
+
+catsAndThings.sale(20);
+insAndOuts.sale(50);
+
+console.log(valleyMall.sales);
